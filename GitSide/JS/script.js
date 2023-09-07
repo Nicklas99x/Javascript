afc621cdeb863a9ec6c82dbadfe638b54d2c77c0
Nicklas Dauer Pedersen <nick5198@edu.ucl.dk>
2023-09-07 11:57:43 +0200

    Calculator virker

--- GitSide/JS/script.js
+++ GitSide/JS/script.js
@@ -6,3 +6,45 @@ function addToList(){
     li.appendChild(document.createTextNode(input));
     ul.append(li);
 }
+
+function addition(){
+    let input = Number(document.getElementById("input").value);
+    let input2 = Number(document.getElementById("input2").value);
+
+    let addition = input + input2;
+
+    printResultToScreen(addition);
+}
+
+function subtraction(){
+    let input = Number(document.getElementById("input").value);
+    let input2 = Number(document.getElementById("input2").value);
+
+    let subtraction = input - input2;
+
+    printResultToScreen(subtraction);
+}
+
+function multiplication(){
+    let input = Number(document.getElementById("input").value);
+    let input2 = Number(document.getElementById("input2").value);
+
+    let multiplication = input * input2;
+
+    printResultToScreen(multiplication);
+}
+
+function division(){
+    let input = Number(document.getElementById("input").value);
+    let input2 = Number(document.getElementById("input2").value);
+
+    let division = input / input2;
+
+    printResultToScreen(division);
+}
+
+function printResultToScreen(result){
+    let output = document.getElementById("output");
+
+    output.append(result);
+}
