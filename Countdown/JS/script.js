bbd8f1e461ee04f4fe0c9ef06d60afe088478758
Nicklas Dauer Pedersen <nick5198@edu.ucl.dk>
2023-09-18 08:21:25 +0200

    commit

--- Countdown/JS/script.js
+++ Countdown/JS/script.js
@@ -6,3 +6,49 @@ function countDown(){
         console.log("gå");
     }
 }
+
+function faculty(n){
+    if(n < 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * faculty(n - 1);
+    }
+}
+
+const n = prompt("indtast et tal");
+const result = faculty(n);
+
+console.log(result);
+
+function facultyButIterative(){
+    const n = prompt("indtast tal");
+
+    let result = 0;
+
+    for(i = 0; i <= n; i++){
+        result *= i;
+    }
+
+    console.log(result);
+}
+
+function cubeFunction(n){ 
+    let dicethrows = [];
+
+    for(i = 0; i < n; i++){
+        let throwDice = Math.floor(Math.random()* 6) + 1;
+        dicethrows[throwDice - 1]++;
+    }
+
+    return dicethrows;
+}
+let m = prompt("throw the cube");
+const results = cubeFunction(m);
+
+console.log(`Results after ${m} throws:`);
+for (let i = 0; i < results.length; i++) {
+  console.log(`Side ${i + 1}: ${results[i]} times`);
+}
